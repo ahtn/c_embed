a13f8526e5b20f4000aabdb7b287a0dd47fa20fd
ahtn <jem@seethis.link>
2018-01-01 22:13:05 +1000

    update comment

--- ring_buf/ring_buf.h
+++ ring_buf/ring_buf.h
@@ -9,7 +9,6 @@
 // Note: A ring buffer defined with a given size `size` will only be able to
 // store `(size-1)` elements at a time, but still uses `size*sizeof(data_type)`
 // bytes in memory. This simplifies the implementation.
-
 #define DEFINE_RING_BUF_TYPE(size, ptr_type, data_type, type_name) \
 typedef struct type_name ## _type { \
     data_type data[size]; \
@@ -83,32 +82,43 @@ static inline void type_name ## _skip (type_name ## _type *buf, ptr_type skip_le
 } \
 
 
-// Note: Define the ring buffer type. Note the resulting data type will take up
-// `(buf_len+1)*sizeof(data_type) + 2*ptr_type` bytes in ram.
-// NOTE: The `ptr_type` should be an unsigned integer type that can hold the
+// This macro defines a ring buffer type and the associated functions to operate
+// on it. It takes the following parameters:
+// * buf_len: the maximum number of elements that can be stored in the ring buffer
+//      at any one time
+// * ptr_type: unsigned integer type that is used for the internal pointers of
+//      the buffer
+// * data_type: type of the elements in the ring_buf
+// * buf_name: name for the ring buffer. The generated functions will be prefixed
+//      with `buf_name` and the ring buffer type itself will be `<buf_name>_type`.
+// Note: Uses `(buf_len+1)*sizeof(data_type) + 2*ptr_type` in RAM for the type.
+// NOTE: The `ptr_type` should be an integer type that can hold the
 // `buf_len` parameter without overflowing. For example:
 // if buf_len == 255 --> ptr_type == uint8_t
 // if buf_len == 256 --> ptr_type == uint16_t
-// NOTE: The compiler should optimize the generated code best when `buf_len==2^n-1`
-// as (since it can optimize `x % (size+1)` best when size+1 is a power of 2)
-#define DEFINE_RING_BUF_VARIANT(buf_len, ptr_type, data_type, type_name) \
-    DEFINE_RING_BUF_TYPE(buf_len+1, ptr_type, data_type, type_name) \
-    DEFINE_RING_BUF_CLEAR_FUNCTION(buf_len+1, ptr_type, data_type, type_name) \
-    DEFINE_RING_BUF_HAS_DATA_FUNCTION(buf_len+1, ptr_type, data_type, type_name) \
-    DEFINE_RING_BUF_LEN_FUNCTION(buf_len+1, ptr_type, data_type, type_name) \
-    DEFINE_RING_BUF_SPACE_FUNCTION(buf_len+1, ptr_type, data_type, type_name) \
-    DEFINE_RING_BUF_GET_FUNCTION(buf_len+1, ptr_type, data_type, type_name) \
-    DEFINE_RING_BUF_PEEK_FUNCTION(buf_len+1, ptr_type, data_type, type_name) \
-    DEFINE_RING_BUF_PUT_FUNCTION(buf_len+1, ptr_type, data_type, type_name) \
-    DEFINE_RING_BUF_FILL_FUNCTION(buf_len+1, ptr_type, data_type, type_name) \
-    DEFINE_RING_BUF_SKIP_FUNCTION(buf_len+1, ptr_type, data_type, type_name) \
-
-// DEFINE_RING_BUF_VARIANT(size, ptr_type, data_type, tname) will define the
-// a ring buffer with the type `tname_type` and the following functions:
-//   tname_clear(*buf): empty the ring buffer
-//   tname_has_data(*buf): check if the ring buffer has data
-//   tname_get(*buf): read one item from the ring buffer
-//   tname_peek(*buf): read one item from the ring buffer without consuming it
-//   tname_put(*buf, uint8_t value): put one item into the ring buffer
-//   tname_fill(*buf, uint8_t *value, uint8_t len): put mutiple items in ring buffer
-//   tname_skip(*buf, uint8_t len): discard `len` items from the ring buffer
+// NOTE: For the better code optimization by the compiler, `ptr_type` should be
+// an unsigned integer. Also, choosing `buf_len` of the form `2^n-1`
+// will result in more efficient code as `(x % (buf_len+1))` can be optimized
+// to `(x & buf_len)`.
+//
+// DEFINE_RING_BUF_VARIANT(buf_len, ptr_type, data_type, buf_name) will define
+// the ring buffer type `<buf_name>_type` and the following functions:
+//   <buf_name>_clear(*buf): empty the ring buffer
+//   <buf_name>_has_data(*buf): check if the ring buffer has data
+//   <buf_name>_get(*buf): read one item from the ring buffer
+//   <buf_name>_peek(*buf): read one item from the ring buffer without consuming it
+//   <buf_name>_put(*buf, data_type value): put one item into the ring buffer
+//   <buf_name>_fill(*buf, data_type *value, ptr_type len): put mutiple items in ring buffer
+//   <buf_name>_skip(*buf, data_type len): discard `len` items from the ring buffer
+//
+#define DEFINE_RING_BUF_VARIANT(buf_len, ptr_type, data_type, buf_name) \
+    DEFINE_RING_BUF_TYPE(buf_len+1, ptr_type, data_type, buf_name) \
+    DEFINE_RING_BUF_CLEAR_FUNCTION(buf_len+1, ptr_type, data_type, buf_name) \
+    DEFINE_RING_BUF_HAS_DATA_FUNCTION(buf_len+1, ptr_type, data_type, buf_name) \
+    DEFINE_RING_BUF_LEN_FUNCTION(buf_len+1, ptr_type, data_type, buf_name) \
+    DEFINE_RING_BUF_SPACE_FUNCTION(buf_len+1, ptr_type, data_type, buf_name) \
+    DEFINE_RING_BUF_GET_FUNCTION(buf_len+1, ptr_type, data_type, buf_name) \
+    DEFINE_RING_BUF_PEEK_FUNCTION(buf_len+1, ptr_type, data_type, buf_name) \
+    DEFINE_RING_BUF_PUT_FUNCTION(buf_len+1, ptr_type, data_type, buf_name) \
+    DEFINE_RING_BUF_FILL_FUNCTION(buf_len+1, ptr_type, data_type, buf_name) \
+    DEFINE_RING_BUF_SKIP_FUNCTION(buf_len+1, ptr_type, data_type, buf_name) \
